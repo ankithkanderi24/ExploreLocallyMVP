a84a6ab5302fa3a6addf309b8bc593f8809507a7
ankithkanderi24 <ankithkanderi@outlook.com>
2023-12-04 14:19:08 -0500

    time of call

--- Frontend/Screens/callscreen.jsx
+++ Frontend/Screens/callscreen.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View } from 'react-native';
 import { ZegoUIKitPrebuiltCall, ONE_ON_ONE_VIDEO_CALL_CONFIG } from '@zegocloud/zego-uikit-prebuilt-call-rn';
 
 function CallPage({ route, navigation }) {
   
   const { username = 'DefaultUsername', isAdvisor } = route.params;
-
   const randomUserID = String(Math.floor(Math.random() * 100000));
+  const [callStartTime, setCallStartTime] = useState(null);
+
+  useEffect(() => {
+    const now = new Date();
+    setCallStartTime(now);
+  }, []);
+
+  const handleHangUp = () => {
+    const now = new Date();
+
+    const duration = (now - callStartTime) / 1000; 
+    console.log(`Call Duration: ${duration} seconds`);
+
+    if (!isAdvisor) {
+      navigation.navigate('RateAdvisor', { username });
+    } else {
+      navigation.navigate('Login');
+    }
+  };
 
   return (
     <View style={{flex: 1}}>
@@ -18,13 +36,7 @@ function CallPage({ route, navigation }) {
         callID='testCallID'
         config={{
           ...ONE_ON_ONE_VIDEO_CALL_CONFIG,
-          onHangUp: () => {
-            if (!isAdvisor) {
-              navigation.navigate('RateAdvisor', { username });
-            } else {
-              navigation.navigate('Login')
-            }
-          },
+          onHangUp: handleHangUp,
         }}
       />
     </View>
